eade319a0ee380114683c6efbed390b0516a99ea
Claudio Bley <claudio.bley@gmail.com>
2018-08-11 01:42:28 +0200

    Add `lookup_const` method to `CLocale` module and use it
    When `setlocale` fails to honor the request, try to look up the symbolic
    category from the defined constants in the CLocale module for better
    error reporting.

--- lib/clocale.rb
+++ lib/clocale.rb
@@ -1,1 +1,15 @@
+# This module defines the `LC_*` constants and locale functions from the linked
+# C library of the current Ruby interpreter.
+#
+# @author Claudio Bley <claudio.bley@gmail.com>
+module CLocale
+  # Looks up the constant corresponding to the given integer value.
+  #
+  # @param int [Integer]
+  # @return [Symbol,nil] the <tt>LC_*</tt> constant as Symbol if found, nil otherwise
+  def self.lookup_const(int)
+    CLocale.constants.find { |s| CLocale.const_get(s) == int }
+  end
+end
+
 require 'clocale/clocale'
