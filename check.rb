da4b6f0bfc62011ca23e1e4be1478be28bd3a202
Claudio Bley <cbley@exa-online.de>
2019-10-07 08:36:02 +0200

    Debug filesystem encoding

--- check.rb
+++ check.rb
@@ -7,12 +7,16 @@ puts arg
 @stats = File.lstat(arg)
 
 if @stats.directory?
-  @contents = Dir.entries(arg)
+  begin
+    @contents = Dir.entries(arg)
 
-  @contents.each do |item|
-    puts "#{item}: #{File.lstat(File.join(arg, item)).size}"
-  rescue => e
-    warn "#{item}: #{e}"
+    @contents.each do |item|
+      puts "#{item}: #{File.lstat(File.join(arg, item)).size}"
+    rescue => e
+      warn "#{item}: #{e}"
+    end
+  rescue StandardError => e
+    puts "#{e}"
   end
 end
 
